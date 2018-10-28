213ba8e48689da68cbc7c381aad5379c5610e9c7
chenwen <chenwen@192.168.157.1>
2018-10-28 22:01:44 +0800

    service层改为注解方式

--- src/com/abc/service/StudentServiceImpl.java
+++ src/com/abc/service/StudentServiceImpl.java
@@ -1,15 +1,22 @@
 package com.abc.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.abc.beans.Student;
 import com.abc.dao.IStudentDao;
 
+@Service("studentService")
 public class StudentServiceImpl implements IStudentService {
+	@Autowired //byType方式
 	private IStudentDao dao;
 	
 	public void setDao(IStudentDao dao) {
 		this.dao = dao;
 	}
-
+	
+	@Transactional
 	@Override
 	public void saveStudent(Student student) {
 		dao.insertStudent(student);
