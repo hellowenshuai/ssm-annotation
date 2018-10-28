3cfde8e561589d02682c75257c998ea89070ac8b
chenwen <chenwen@192.168.157.1>
2018-10-28 22:05:22 +0800

    dao层改为注解

--- src/com/abc/dao/IStudentDao.java
+++ src/com/abc/dao/IStudentDao.java
@@ -1,9 +1,11 @@
 package com.abc.dao;
 
+import org.apache.ibatis.annotations.Insert;
+
 import com.abc.beans.Student;
 
 public interface IStudentDao {
-
+	@Insert("insert into student(name, age) values(#{name}, #{age})")
 	void insertStudent(Student student);
 
 }
